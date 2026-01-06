f99df2c5bf32ce7b59b01b2c77649aecbce47482
Crypoune <a.messenet@gmail.com>
2026-01-06 15:04:01 +0100

    feat: implement command execution logic using execve

--- exec.c
+++ exec.c
@@ -3,37 +3,40 @@
 /**
  * execute_cmd - execute a command
  * @argv: argument vector
+ * @prog_name: name of the program
+ * @cmd_count: command count for error messages
+ *
+ * Return: 1 on success, 127 if command not found, other on error
  */
-void execute_cmd(char **argv)
+int execute_cmd(char **argv, char *prog_name, int cmd_count)
 {
 	pid_t pid;
+	int status;
 	char *cmd_path;
 
 	cmd_path = find_cmd(argv[0]);
 	if (!cmd_path)
 	{
-		dprintf(2, "./shell: %s: not found\n", argv[0]);
-		return;
+		dprintf(2, "%s: %d: %s: not found\n",
+				prog_name, cmd_count, argv[0]);
+		return (127);
 	}
 
 	pid = fork();
-	if (pid == -1)
-	{
-		perror("fork");
-		free(cmd_path);
-		return;
-	}
-
 	if (pid == 0)
 	{
 		execve(cmd_path, argv, environ);
 		perror("execve");
 		exit(EXIT_FAILURE);
 	}
-	else
+	else if (pid < 0)
 	{
-		waitpid(pid, NULL, 0);
+		perror("fork");
+		free(cmd_path);
+		return (1);
 	}
 
+	waitpid(pid, &status, 0);
 	free(cmd_path);
+	return (1);
 }
