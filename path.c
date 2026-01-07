5e94a347824d10d21cfc63b5e6e4016e1288c63b
Crypoune <a.messenet@gmail.com>
2026-01-07 10:39:47 +0100

    add cmd_error variables

--- path.c
+++ path.c
@@ -1,5 +1,7 @@
 #include "shell.h"
 
+int cmd_error = 0;
+
 /**
  * find_cmd - Trouve le chemin complet d'une commande
  * @cmd: La commande à chercher
@@ -12,28 +14,39 @@ char *find_cmd(char *cmd)
 	char full_path[1024];
 	struct stat st;
 
+	cmd_error = 0;
+
+
 	if (!cmd || cmd[0] == '\0')
+	{
+		cmd_error = 127;
 		return (NULL);
+	}
 
 	/* If absolute or relative path */
 	if (strchr(cmd, '/'))
 	{
 		if (stat(cmd, &st) != 0)
+		{
+			cmd_error = 127;
 			return (NULL);
+		}
 
-		if (S_ISDIR(st.st_mode))
-			return (strdup("/IS_A_DIR"));
-
-		if (access(cmd, X_OK) != 0)
-			return (strdup("/NO_PERMISSION"));
-
+		if (S_ISDIR(st.st_mode) || access(cmd, X_OK) != 0)
+		{
+			cmd_error = 126;
+			return (NULL);
+		}
 		return (strdup(cmd));
 	}
 
 	/* Search in PATH */
 	path = getenv("PATH");
 	if (!path || path[0] == '\0')
+	{
+		cmd_error = 127;
 		return (NULL);
+	}
 
 	path_copy = strdup(path);
 	if (!path_copy)
@@ -42,23 +55,24 @@ char *find_cmd(char *cmd)
 	dir = strtok(path_copy, ":");
 	while (dir)
 	{
-		if (dir[0] == '\0')
+		if (*dir == '\0')
 			dir = ".";
 
 		snprintf(full_path, sizeof(full_path), "%s/%s", dir, cmd);
 		if (stat(full_path, &st) == 0)
 		{
-			if (!S_ISDIR(st.st_mode)
-			&& (access(full_path, X_OK) == 0))
+			free(path_copy);
+			if (S_ISDIR(st.st_mode) || access(full_path, X_OK) != 0)
 			{
-				free(path_copy);
-				return (strdup(full_path));
+				cmd_error = 126;
+				return (NULL);
 			}
+			return (strdup(full_path));
 		}
-
 		dir = strtok(NULL, ":");
 	}
 
 	free(path_copy);
+	cmd_error = 127;
 	return (NULL);
 }
