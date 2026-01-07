04b9039ffd61b34578ec27ec40a000846ff5eafe
Crypoune <a.messenet@gmail.com>
2026-01-07 18:38:42 +0100

    refactor: split execute_cmd into static functions to comply with betty

--- exec.c
+++ exec.c
@@ -1,68 +1,141 @@
 #include "shell.h"
 
+static int check_cmd_error(char *cmd, char *prog_name, int cmd_count)
+{
+	struct stat st;
+
+	if (!cmd)
+		return (127);
+
+	if (strchr(cmd, '/'))
+	{
+		if (stat(cmd, &st) != 0)
+		{
+			dprintf(2, "%s: %d: %s: not found\n",
+				prog_name, cmd_count, cmd);
+			return (127);
+		}
+
+		if (S_ISDIR(st.st_mode) || access(cmd, X_OK) != 0)
+		{
+			dprintf(2, "%s: %d: %s: Permission denied\n",
+				prog_name, cmd_count, cmd);
+			return (126);
+		}
+
+		return (0);
+	}
+
+	if (!find_cmd(cmd))
+	{
+		dprintf(2, "%s: %d: %s: not found\n",
+			prog_name, cmd_count, cmd);
+		return (127);
+	}
+
+	return (0);
+}
+
+/**
+ * get_cmd_path - get the full path of a command
+ * @cmd: command name
+ * @prog_name: program name (shell)
+ * @cmd_count: command count (for error messages)
+ *
+ * Return: full path of the command, or NULL if not found
+ */
+static char *get_cmd_path(char *cmd, char *prog_name, int cmd_count)
+{
+	struct stat st;
+	char *cmd_path;
+
+	if (!cmd)
+		return (NULL);
+
+	if (strchr(cmd, '/'))
+	{
+		if (stat(cmd, &st) != 0)
+		{
+			dprintf(2, "%s: %d: %s: not found\n",
+				prog_name, cmd_count, cmd);
+			return (NULL);
+		}
+		if (S_ISDIR(st.st_mode) || access(cmd, X_OK) != 0)
+		{
+			dprintf(2, "%s: %d: %s: Permission denied\n",
+				prog_name, cmd_count, cmd);
+			return (NULL);
+		}
+
+		return (strdup(cmd));
+	}
+
+	cmd_path = find_cmd(cmd);
+	if (!cmd_path)
+		dprintf(2, "%s: %d: %s: not found\n",
+			prog_name, cmd_count, cmd);
+
+	return (cmd_path);
+}
+
+/**
+ * handle_exit_status - handle the exit status
+ * @status: exit status of the child process
+ *
+ * Return: appropriate exit code
+ */
+static int handle_exit_status(int status)
+{
+	if (WIFEXITED(status))
+		return (WEXITSTATUS(status));
+	else if (WIFSIGNALED(status))
+		return (128 + WTERMSIG(status));
+	return (1);
+}
+
 /**
  * execute_cmd - Exécute une commande externe
  * @argv: Tableau des arguments de la commande
  * @prog_name: Nom du programme (shell)
  * @cmd_count: Numéro de la commande (pour les messages d'erreur)
+ *
  * Return: 1 en cas de succès, code d'erreur sinon
  */
 int execute_cmd(char **argv, char *prog_name, int cmd_count)
 {
 	pid_t pid;
 	int status;
+	int error_code;
 	char *cmd_path;
-	struct stat st;
 
 	if (!argv[0])
 		return (127);
 
+	error_code = check_cmd_error(argv[0], prog_name, cmd_count);
+	if (error_code != 0)
+		return (error_code);
 
-	if (strchr(argv[0], '/'))
-	{
-		if (stat(argv[0], &st) != 0)
-		{
-			dprintf(2, "%s: %d: %s: not found\n", prog_name, cmd_count, argv[0]);
-			return (127);
-		}
+	cmd_path = get_cmd_path(argv[0], prog_name, cmd_count);
+	if (!cmd_path)
+		return (127);
 
-		if (S_ISDIR(st.st_mode) || access(argv[0], X_OK) != 0)
-		{
-			dprintf(2, "%s: %d: %s: Permission denied\n",
-				prog_name, cmd_count, argv[0]);
-			return (126);
-		}
-
-		cmd_path = strdup(argv[0]);
-	}
-	else
-	{
-		cmd_path = find_cmd(argv[0]);
-		if (!cmd_path)
-		{
-			dprintf(2, "%s: %d: %s: not found\n", prog_name, cmd_count, argv[0]);
-			return (127);
-		}
-	}
-
-	if (stat(cmd_path, &st) == 0 &&
-		(S_ISDIR(st.st_mode) || access(cmd_path, X_OK) != 0))
-	{
-		dprintf(2, "%s: %d: %s: Permission denied\n",
-				prog_name, cmd_count, argv[0]);
-		free(cmd_path);
-		return (126);
-	}
-
-	/* Execute the command */
 	pid = fork();
+	if (pid == -1)
+	{
+		perror(prog_name);
+		free(cmd_path);
+		return (1);
+	}
+
 	if (pid == 0)
 	{
 		execve(cmd_path, argv, environ);
 		perror(prog_name);
-		exit(1);
+		exit(127);
 	}
+
 	waitpid(pid, &status, 0);
 	free(cmd_path);
-	return (WEXITSTATUS(status));
+
+	return (handle_exit_status(status));
 }
