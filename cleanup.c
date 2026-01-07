e9d9db2347f5cdf0a237c08898f333a930881ac8
Crypoune <a.messenet@gmail.com>
2026-01-07 18:35:08 +0100

    feat: add free_argv function to free argument arrays

---
+++ cleanup.c
@@ -0,0 +1,21 @@
+#include "shell.h"
+
+/**
+ * free_argv - free memory allocated for argument vector
+ * @argv: argument vector to free
+ *
+ * Return: void
+ */
+void free_argv(char **argv)
+{
+	int i = 0;
+
+	if (!argv)
+		return;
+		while (argv[i])
+	{
+		free(argv[i]);
+		i++;
+	}
+	free(argv);
+}
