906f3defe488e7eaa6fabd5885c8400f23d89ead
Crypoune <a.messenet@gmail.com>
2026-01-07 22:40:00 +0100

    refactor: get rid of MAX_PATH, use dynamic allocation instead

--- path.c
+++ path.c
@@ -1,6 +1,31 @@
 #include "shell.h"
 
 /**
+ * concatenate_path - concatenate directory and command to form full path
+ * @dir: directory path
+ * @cmd: command name
+ *
+ * Return: full path string, or NULL on failure
+ */
+static char *concatenate_path(const char *dir, const char *cmd)
+{
+	char *full_path;
+
+	if (!dir || !cmd)
+		return (NULL);
+
+	full_path = malloc(strlen(dir) + strlen(cmd) + 2);
+	if (!full_path)
+		return (NULL);
+
+	strcpy(full_path, dir);
+	strcat(full_path, "/");
+	strcat(full_path, cmd);
+
+	return (full_path);
+}
+
+/**
  * find_cmd - Find the full path of a command
  * @cmd: Command name
  *
@@ -9,7 +34,7 @@
 char *find_cmd(char *cmd)
 {
 	char *path, *path_copy, *dir;
-	char full_path[PATH_MAX];
+	char *full_path;
 	struct stat st;
 
 	if (!cmd || cmd[0] == '\0')
@@ -20,7 +45,6 @@ char *find_cmd(char *cmd)
 			return (NULL);
 		return (strdup(cmd));
 	}
-
 	path = getenv("PATH");
 	if (!path || !*path)
 		return (NULL);
@@ -32,18 +56,19 @@ char *find_cmd(char *cmd)
 	{
 		if (*dir == '\0')
 			dir = ".";
-
-		strcpy(full_path, dir);
-		strcat(full_path, "/");
-		strcat(full_path, cmd);
-
-		if (stat(full_path, &st) == 0 &&
-			!S_ISDIR(st.st_mode) &&
-			access(full_path, X_OK) == 0)
+		full_path = concatenate_path(dir, cmd);
+		if (!full_path)
 		{
 			free(path_copy);
-			return (strdup(full_path));
+			return (NULL);
 		}
+		if (stat(full_path, &st) == 0 && !S_ISDIR(st.st_mode)
+		&& access(full_path, X_OK) == 0)
+		{
+			free(path_copy);
+			return (full_path);
+		}
+		free(full_path);
 		dir = strtok(NULL, ":");
 	}
 	free(path_copy);
