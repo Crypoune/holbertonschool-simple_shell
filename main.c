ed00e0acf72d12e43c04b8615791946232061d46
Crypoune <a.messenet@gmail.com>
2026-01-07 14:35:38 +0100

    fix: display prompt before reading input in interactive mode

--- main.c
+++ main.c
@@ -13,15 +13,18 @@ int main(int argc, char **argv)
 	size_t len = 0;
 	ssize_t read;
 	char **args;
-	int status = 0;
 	int cmd_count = 0;
+	int interactive = 0;
 
 	(void)argc;
 
-	int interactive = isatty(STDIN_FILENO);
+	interactive = isatty(STDIN_FILENO);
 
 	while (1)
 	{
+		if (interactive)
+			write(1, "$ ", 2);
+
 		read = getline(&line, &len, stdin);
 
 		if (read == -1)
@@ -30,9 +33,6 @@ int main(int argc, char **argv)
 				write(1, "\n", 1);
 			break;
 		}
-
-		if (interactive)
-			write(1, "$ ", 2);
 
 		if (read == 1)
 			continue;
@@ -53,7 +53,8 @@ int main(int argc, char **argv)
 		}
 
 		cmd_count++;
-		status = execute_cmd(args, argv[0], cmd_count);
+
+		execute_cmd(args, argv[0], cmd_count);
 		free(args);
 	}
 
