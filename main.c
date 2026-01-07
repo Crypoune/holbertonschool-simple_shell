9f7d71ee4c0ecee8b2d0214bed74e342846fc94d
Crypoune <a.messenet@gmail.com>
2026-01-07 18:33:30 +0100

    fix: increment cmd_count after validation and use free_argv for cleanup

--- main.c
+++ main.c
@@ -14,51 +14,39 @@ int main(int argc, char **argv)
 	ssize_t read;
 	char **args;
 	int cmd_count = 0;
-	int interactive = 0;
-	int status = 0;
+	int interactive = 0, status = 0;
 
 	(void)argc;
-
 	interactive = isatty(STDIN_FILENO);
-
 	while (1)
 	{
 		if (interactive)
 			write(1, "$ ", 2);
-
 		read = getline(&line, &len, stdin);
-
 		cmd_count++;
-
 		if (read == -1)
 		{
 			if (interactive)
 				write(1, "\n", 1);
 			break;
 		}
-
 		if (read == 1)
 			continue;
-
 		line[read - 1] = '\0';
-
 		args = parse_line(line);
 		if (!args || !args[0])
 		{
-			free(args);
+			free_argv(args);
 			continue;
 		}
-
 		if (handle_builtins(args))
 		{
-			free(args);
+			free_argv(args);
 			continue;
 		}
-
 		status = execute_cmd(args, argv[0], cmd_count);
-		free(args);
+		free_argv(args);
 	}
-
 	free(line);
 	return (status);
 }
