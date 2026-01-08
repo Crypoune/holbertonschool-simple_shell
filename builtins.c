14a5a0f75d86210cbccddfbb77fdb4c8b831a5f5
Crypoune <a.messenet@gmail.com>
2026-01-08 16:55:57 +0100

    feat: add exit status handling and improve env command

--- builtins.c
+++ builtins.c
@@ -3,23 +3,27 @@
 /**
  * handle_builtins - handle built-in commands
  * @argv: Array of command arguments
+ * @status: Pointer to the exit status
  *
  * Return: 1 if a builtin was executed, 0 otherwise
  */
-int handle_builtins(char **argv)
+int handle_builtins(char **argv, int *status)
 {
 	int i = 0;
+	int exit_code;
 
 	if (!argv || !argv[0])
-	{
 		return (0);
-	}
 
 	/* exit built-in */
 	if (strcmp(argv[0], "exit") == 0)
 	{
-		(void) argv;
-		return (-1);
+		if (argv[1])
+		{
+			exit_code = atoi(argv[1]);
+			exit(exit_code);
+		}
+		exit(*status);
 	}
 
 	/* env built-in */
@@ -30,6 +34,7 @@ int handle_builtins(char **argv)
 			printf("%s\n", environ[i]);
 			i++;
 		}
+		*status = 0;
 		return (1);
 	}
 
